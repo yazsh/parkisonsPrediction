ce979091314ae6e3550ad2e8d733669ff5f26fed
Yazen2017 <yazen.shunnar@gmail.com>
2017-09-04 20:15:16 -0400

    Update commit

--- dataCleaning.py
+++ dataCleaning.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import csv
 
+data = pd.read_csv("/Users/yazen/Desktop/datasets/ParkinsonsData/Drinking_all_export.txt", chunksize=1000, iterator=True, nrows=5)
 
-pd.read_csv("/Users/yazen/Desktop/atomprojects/parkisonsPrediction/dataCleaning.py", nrow=10)
+for chunk in data:
+    print(chunk)
